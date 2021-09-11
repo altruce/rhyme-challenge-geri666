997eb2c155b3cfac6639bb2accb236036f3f1b38
g.szijarto.inf19 <g.szijarto.inf19@stud.bbbaden.ch>
2021-09-11 15:54:50 +0200

    my solution

--- src/main/java/com/altruce/challenge/Main.java
+++ src/main/java/com/altruce/challenge/Main.java
@@ -1,6 +1,14 @@
 package com.altruce.challenge;
 
 import org.json.simple.JSONObject;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+
+/*
+Altruce GmbH
+modified by gergely szijarto, 11.9.2021
+ */
 
 public class Main {
 
@@ -10,10 +18,35 @@ public class Main {
             JSONReader jsonReader = new JSONReader();
             JSONObject rhymes = jsonReader.readFile("src/main/resources/rhymes.json");
             String input = String.join(" ", args);
+            // String input = "Als ich aufsah\nwar niemand mehr _\n\nEs war, als hätt' der
+            // Himmel\ndie Erde still geküsst,\nDass sie im _\nVon Ihm nun träumen _\n\nDie
+            // Luft ging durch die Felder,\nDie Ähren wogten sacht,\nEs rauschten leis' die
+            // _\nSo sternklar war die _";
+            BufferedReader bufReader = new BufferedReader(new StringReader(input));
+            String rhyme = null;
+            String line = null;
+            String newLine = null;
+            int i = 0;
+            ArrayList<String> rhymeList = new ArrayList<>();
 
-            // TODO - complete rhymes
+            while ((line = bufReader.readLine()) != null) {
+                if (line.equals("")) {
+                    completedRhymes += "\n";
+                    continue;
+                }
 
-            // TODO - export / illustrate rhymes
+                String lastWord = line.substring(line.lastIndexOf(' ') + 1, line.length()).replaceAll(",", "");
+
+                if (!lastWord.equals("_")) {
+                    completedRhymes += line + "\n";
+                    rhyme = getRhyme(lastWord, rhymes);
+                    rhymeList.add(rhyme);
+                } else {
+                    newLine = line.replace("_", rhymeList.get(i++));
+                    completedRhymes += newLine + "\n";
+                }
+
+            }
 
             // even if you created a fancy export / illustration from the completed rhymes
             // we need this print to run the automated grading test
@@ -22,5 +55,15 @@ public class Main {
             System.out.println(e.getMessage());
             System.exit(1);
         }
+
     }
+
+    public static String getRhyme(String firstWord, JSONObject rhymes) {
+        return (String) rhymes.get(firstWord);
+    }
+
+    public static int getCharPos(String input, String chars) {
+        return input.indexOf(chars);
+    }
+
 }
